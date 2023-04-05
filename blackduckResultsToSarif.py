bd191cba83ab88b5b161cbcdf96e38f644e90740
Jouni Lehto <lehto.jouni@gmail.com>
2023-04-05 19:02:45 +0300

    modified help text

--- blackduckResultsToSarif.py
+++ blackduckResultsToSarif.py
@@ -127,7 +127,7 @@ def getHelpMarkdown(vulnerability):
         cve_link = f'[View CVE record]({vulnerability["_meta"]["href"]})'
     elif getLinksparam(vulnerability, "related-vulnerabilities", "label") == "NVD":
         cve_link = f'[View CVE record]({getLinksparam(vulnerability, "related-vulnerabilities", "href")})'
-    messageText = f'| Description | \n| :----------- |\n| {vulnerability["description"] if vulnerability["description"] else "-"} |\n| {bdsa_link if bdsa_link else ""}{cve_link if cve_link else ""} |\n\n## Base Score Metrics (CVSS v3.x Metrics)\n|  |  |  |  |\n| :-- | :-- | :-- | :-- |\n| Attack vector | **{attackVector}** | Availability | **{availabilityImpact}** |\n| Attack complexity | **{attackComplexity}** | Confidentiality | **{confidentialityImpact}** |\n| Integrity | **{integrityImpact}** | Scope | **{scope}** |\n| Privileges required | **{privilegesRequired}** | User interaction | **{userInteraction}** |\n\n{vector}'
+    messageText = f'## Description\n {vulnerability["description"] if vulnerability["description"] else "-"}\n{bdsa_link if bdsa_link else ""}{cve_link if cve_link else ""}\n\n## Base Score Metrics (CVSS v3.x Metrics)\n|  |  |  |  |\n| :-- | :-- | :-- | :-- |\n| Attack vector | **{attackVector}** | Availability | **{availabilityImpact}** |\n| Attack complexity | **{attackComplexity}** | Confidentiality | **{confidentialityImpact}** |\n| Integrity | **{integrityImpact}** | Scope | **{scope}** |\n| Privileges required | **{privilegesRequired}** | User interaction | **{userInteraction}** |\n\n{vector}'
     return messageText
 
 
